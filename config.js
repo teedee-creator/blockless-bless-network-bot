6f13ce2eff13c9e70322492a42d25d0ba62b2811
teedee-creator <60574564+teedee-creator@users.noreply.github.com>
2025-01-03 23:33:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,8 @@ module.exports = [
     {
         usertoken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Nzc3MDQ2MmMzMWVhZjNkODUzN2Q2MjgiLCJwdWJsaWNBZGRyZXNzIjoiOHpaVlQ3cXBNZFJwOU56aU16Z0VqVVlCU2NQWXZqMjRkQkFmVVNIMjNHZVoiLCJ3YWxsZXRUeXBlIjoic29sYW5hIiwiaWF0IjoxNzM1ODUzMTU1LCJleHAiOjE3Njc0MTA3NTV9.HPUkjpWWxrBX693lLJ36h0CiH_g3vYcVWUcFdRsGGIE',
         nodes: [
-            { nodeId: '12D3KooWR7vhmwdQoPsKRnR1eFyy2GTMXVdqp8wmDjJjy1AfZQfb', hardwareId: '984adeaeb7e29e8483258a5061e63d860f02f4fe730ace6ed5ca3e7e094d8faf', proxy: 'proxy1' },
-            { nodeId: '12D3KooWR7vhmwdQoPsKRnR1eFyy2GTMXVdqp8wmDjJjy1AfZQfb', hardwareId: '984adeaeb7e29e8483258a5061e63d860f02f4fe730ace6ed5ca3e7e094d8faf', proxy: 'proxy2' },
-            { nodeId: '12D3KooWR7vhmwdQoPsKRnR1eFyy2GTMXVdqp8wmDjJjy1AfZQfb', hardwareId: '984adeaeb7e29e8483258a5061e63d860f02f4fe730ace6ed5ca3e7e094d8faf', proxy: 'proxy3' },
-            { nodeId: '12D3KooWR7vhmwdQoPsKRnR1eFyy2GTMXVdqp8wmDjJjy1AfZQfb', hardwareId: '984adeaeb7e29e8483258a5061e63d860f02f4fe730ace6ed5ca3e7e094d8faf', proxy: 'proxy4' },
-            { nodeId: '12D3KooWR7vhmwdQoPsKRnR1eFyy2GTMXVdqp8wmDjJjy1AfZQfb', hardwareId: '984adeaeb7e29e8483258a5061e63d860f02f4fe730ace6ed5ca3e7e094d8faf', proxy: 'proxy5' }
+            { nodeId: '12D3KooWR7vhmwdQoPsKRnR1eFyy2GTMXVdqp8wmDjJjy1AfZQfb', hardwareId: '984adeaeb7e29e8483258a5061e63d860f02f4fe730ace6ed5ca3e7e094d8faf', proxy: 'proxy1' }
+             
         ]
     },
    
